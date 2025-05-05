f113896fcc3af90956ea7f7a09ad7d4c5c02311f
marckstar-RjJ <202001810@est.umss.edu>
2025-05-05 09:31:20 -0400

    arreglos 05/05

--- src/services/userService.js
+++ src/services/userService.js
@@ -1,20 +1,18 @@
-import axios from 'axios';
-
-const API_URL = import.meta.env.VITE_API_URL;
+import api from '../config/api';
 
 export const registrarUsuario = async (datos) => {
-  return await axios.post(`${API_URL}/users/register`, datos);
+  return await api.post('/users/register', datos);
 };
 
 export const loginUsuario = async ({ email, password }) => {
-  return await axios.post(`${API_URL}/users/login`, { email, password });
+  return await api.post('/users/login', { email, password });
 };
 
 export const solicitarRecuperacion = async (email) => {
-    return await axios.post(`${API_URL}/users/forgot-password`, { email });
-  };
-  
-  export const resetearContrasena = async (token, nuevaPassword) => {
-    return await axios.post(`${API_URL}/users/reset-password`, { token, nuevaPassword });
-  };
+  return await api.post('/users/forgot-password', { email });
+};
+
+export const resetearContrasena = async (token, nuevaPassword) => {
+  return await api.post('/users/reset-password', { token, nuevaPassword });
+};
   
