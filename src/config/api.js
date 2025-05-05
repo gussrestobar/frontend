619f07d9d0ffb3eba26d7f7f53c49eaf4a3338d6
marckstar-RjJ <202001810@est.umss.edu>
2025-05-05 10:09:21 -0400

    arreglos finales 3

--- src/config/api.js
+++ src/config/api.js
@@ -2,8 +2,8 @@ import axios from 'axios';
 
 const API_URL = import.meta.env.VITE_API_URL;
 
-// Asegurarse de que la URL base no termine con /api
-const baseURL = API_URL.endsWith('/api') ? API_URL.slice(0, -4) : API_URL;
+// Asegurarse de que la URL base termine con /api
+const baseURL = API_URL.endsWith('/api') ? API_URL : `${API_URL}/api`;
 
 const api = axios.create({
   baseURL,
